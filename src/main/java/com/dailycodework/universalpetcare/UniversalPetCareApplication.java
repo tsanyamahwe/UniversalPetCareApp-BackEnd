7c70119de35c64054c83ac95af1ed842e2d3724a
Tendai Sanyamahwe <TSanyamahwe@dynamicdna.co.za>
2025-05-28 13:31:50 +0200

    petcareapp

---
+++ src/main/java/com/dailycodework/universalpetcare/UniversalPetCareApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodework.universalpetcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalPetCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalPetCareApplication.class, args);
+	}
+
+}
